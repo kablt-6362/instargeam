2825f15fcc5b73c8e6acdc76bbbe5c5ce552ad97
kablt--6362 <wogus6362@gmail.com>
2025-12-08 16:44:18 +0900

    feed 기능 구현, 새로운 home.html

--- src/main/java/com/example/instagram/controller/HomeController.java
+++ src/main/java/com/example/instagram/controller/HomeController.java
@@ -1,13 +1,9 @@
 package com.example.instagram.controller;
 
-import com.example.instagram.dto.response.PostResponse;
 import com.example.instagram.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,13 +12,18 @@ public class HomeController {
     private final PostService postService;
 
 
-    @GetMapping("/")
-    public String home(Model model)
-    {
-//        List<PostResponse> posts = postService.getAllPosts();
-        List<PostResponse> posts = postService.getAllPostWithStats();
-        model.addAttribute("posts",posts);
+//    @GetMapping("/")
+//    public String home(Model model)
+//    {
+////        List<PostResponse> posts = postService.getAllPosts();
+//        List<PostResponse> posts = postService.getAllPostWithStats();
+//        model.addAttribute("posts",posts);
+//        return "home";
+//    }
 
+    @GetMapping("/")
+    public String home(){
         return "home";
     }
+
 }
