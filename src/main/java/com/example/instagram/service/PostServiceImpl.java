bbd8af86fbf9fed07fb807e8d27c5032ca0a4cc9
kablt--6362 <wogus6362@gmail.com>
2025-12-08 16:24:39 +0900

    Feed 기능 구현을 위한 Repository, Service 작성

--- src/main/java/com/example/instagram/service/PostServiceImpl.java
+++ src/main/java/com/example/instagram/service/PostServiceImpl.java
@@ -5,9 +5,13 @@ import com.example.instagram.dto.response.PostResponse;
 import com.example.instagram.entity.Post;
 import com.example.instagram.entity.User;
 import com.example.instagram.repository.CommentRepository;
+import com.example.instagram.repository.FollowRepository;
 import com.example.instagram.repository.LikeRepository;
 import com.example.instagram.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,6 +29,7 @@ public class PostServiceImpl implements PostService {
     private final LikeRepository likeRepository;
     private final CommentRepository commentRepository;
     private final FileService fileService;
+    private final FollowRepository followRepository;
 
     @Override
     @Transactional
@@ -95,6 +100,40 @@ public class PostServiceImpl implements PostService {
                     return PostResponse.from(post,commentCount,likeCount);
                         })
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Slice<PostResponse> getFeedPosts(Long userId, Pageable pageable){
+        List<Long> followingIds = followRepository.findFollowingIdsByFollowerId(userId);
+
+        Slice<Post> posts = postRepository.findFeedPostsByUserIds(followingIds,pageable);
+
+        List<PostResponse> content = posts.getContent().stream()
+                .map(post -> {
+                    long likeCount = likeRepository.countByPostId(post.getId());
+                    long commnetCount = commentRepository.countByPostId(post.getId());
+                    return PostResponse.from(post, commnetCount, likeCount);
+                })
+                .toList();
+
+        return new SliceImpl<>(content, pageable, posts.hasNext());
+
+    };
+
+    @Override
+    public Slice<PostResponse> getAllPostsPaging(Pageable pageable){
+        Slice<Post> posts = postRepository.findAllWithUserPaging(pageable);
+
+        List<PostResponse> content = posts.getContent().stream()
+                .map(post -> {
+                    long likeCount = likeRepository.countByPostId(post.getId());
+                    long commentCount = commentRepository.countByPostId(post.getId());
+
+                    return PostResponse.from(post, commentCount, likeCount);
+                })
+                .toList();
+
+            return new SliceImpl<>(content, pageable, posts.hasNext());
     }
 
 }
